09d40eb2342e951e0209764a8a1e90063484c587
abigailhuertadev <abi.1816@gmail.com>
2024-07-11 20:53:28 -0600

    Mi sitio Web
    Para evaluacion de mi curso

---
+++ js/site.js
@@ -0,0 +1,37 @@
+function toggleMenu(){
+    var button = document.querySelector('.gg-menu-oreos');
+    button.classList.toggle('gg-close');
+
+    var menu = document.getElementsByClassName('js-menu');
+    menu[0].classList.toggle("active");
+}
+
+function fixPhotoOnScroll(){
+    var photo = document.getElementById("photo-profile");
+    // Operador ternario
+    this.scrollY > 50 ? photo.style.marginTop = 0 : photo.style.marginTop = '-7rem';
+    
+    /*
+    if(this.scrollY > 50){
+        //Cuando se cumple la condicion
+        photo.style.marginTop = 0;
+    }
+    else{
+        //cuando no se cumple
+        photo.style.marginTop = '-7rem';
+    }
+    */
+}
+
+window.addEventListener("scroll",  fixPhotoOnScroll, false);
+
+function showImage(image){
+    var newImage = document.createElement('img');
+    newImage.src = image.src;
+    newImage.removeAttribute('onclick');
+    var modal = document.getElementById('preview');
+    modal.classList.toggle('showModal');
+    modal.appendChild(newImage); 
+}
+
+
